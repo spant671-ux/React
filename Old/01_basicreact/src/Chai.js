7cea77b7f5a47e8e981215a2a73d25b3a2dd6f6f
Santosh <spant671@gmail.com>
2026-09-17 19:00:14 +0530

    react

--- Old/01_basicreact/src/Chai.js
+++
@@ -1,6 +0,0 @@
-function Chai(){
-    return(
-        <h2>Chai in React</h2>
-    )
-}
-export default Chai;
--- Old/01_vitereact/src/chai.jsx
+++
@@ -1,7 +0,0 @@
-function Chai(){
-    return(
-        <h2>Another Line in React </h2>
-    )
-}
-
-export default Chai
