04b92f7228cb4cebe05df76b9508cb5ebf1e0d71
Santosh <spant671@gmail.com>
2026-09-20 18:03:47 +0530

    counter

--- Old/02counter/src/App.jsx
+++ Old/02counter/src/App.jsx
@@ -1,12 +1,24 @@
 import './App.css'
+import {useState } from 'react'
 
 function App() {
 
-  let counter = 15;
+  let [counter, setCounter] = useState(15)
+
+  // let counter = 15;
 
   const addValue = () =>{
+    
+    // counter = counter+1;
+    setCounter(counter + 1)
     console.log('clicked', counter)
-    counter = counter+1;
+  }
+
+  const removeValue = () =>{
+    
+    // counter = counter+1;
+    setCounter(counter - 1)
+    console.log('clicked', counter)
   }
 
   return (
@@ -14,9 +26,10 @@ function App() {
     <h1>Chai aur React</h1>
     <h2>Counter Value: {counter}</h2>
 
-    <button onClick={addValue}>Add Value</button>
+    <button onClick={addValue}>Add Value {counter} </button>
     <br />
-    <button>Remove Value</button>
+    <button onClick={removeValue}>Remove Value {counter} </button>
+    <p>Footer: {counter} </p>
     </>
   )
 }
