f248ef2a0632fd3e927eff5425418a8fc861430e
Santosh <spant671@gmail.com>
2026-09-22 03:21:16 +0530

    tailwindcss and props

--- 03tailwind-props/src/App.jsx
+++ 03tailwind-props/src/App.jsx
@@ -2,10 +2,12 @@ import Card from './components/Card'
 import './App.css'
 
 function App() {
+
   return (
     <>
     <h1 className='bg-green-400 text-black p-4 rounded-xl '>Tailwind Test</h1>
-    <Card channel= "chai" />
+    <Card username= "chai" btnText="click me" />
+    <Card username= "sans " btnText="visit me" />
 
     </>
   )
--- 03tailwind-props/src/components/Card.jsx
+++ 03tailwind-props/src/components/Card.jsx
@@ -1,6 +1,6 @@
 
-function Card(props) {
-    console.log('props', props)
+function Card({username, btnText="visit me"}) {
+    console.log(username)
   return (
     <div className="max-w-xs rounded-md shadow-md bg-black text-gray-100">
       <img
@@ -10,7 +10,7 @@ function Card(props) {
       />
       <div className="flex flex-col justify-between p-6 space-y-8">
         <div className="space-y-2">
-          <h2 className="text-3xl font-semibold tracking-wide">Lorem</h2>
+          <h2 className="text-3xl font-semibold tracking-wide">{username}</h2>
           <p className="text-gray-400">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio
             tempora ipsum soluta amet corporis accusantium aliquid consectetur
@@ -21,7 +21,7 @@ function Card(props) {
           type="button"
           className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-gray-800 text-gray-200"
         >
-          Read more
+          {btnText}
         </button>
       </div>
     </div>
